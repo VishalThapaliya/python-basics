77d49ce98661294b6dd7ac8732af34412b1d0493
VishalThapaliya <vishal.thapaliya@gmail.com>
2026-09-08 21:07:28 +0200

    feat(day_2): basics of string functions

---
+++ day_02/lab_004.py
@@ -0,0 +1,15 @@
+# String functions
+
+my_str = "I am learning Python"
+
+# String slicing
+print(my_str[0: 10]) # 0 = starting point, 10 = how many characters
+
+# String length
+print(len(my_str))
+
+# Show string from a specific point
+print(my_str[8:len(my_str)])
+
+# Reverse a string
+print(my_str[::-1])
