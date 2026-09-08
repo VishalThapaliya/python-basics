f58ee8d9584f77ab09e824c8e24b92ce051d4934
VishalThapaliya <vishal.thapaliya@gmail.com>
2026-09-08 22:13:06 +0200

    feat(calc): calculate square and cube of a number

---
+++ day_02/lab_008.py
@@ -0,0 +1,8 @@
+# Develop a Python script that calculates the square and cube of a given number.
+num = int(input("Enter a number: "))
+
+square = num ** 2
+cube = num ** 3
+
+print(f"Square = {square}")
+print(f"Cube = {cube}")
